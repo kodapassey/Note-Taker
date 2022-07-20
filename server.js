b4a04a1772ca66453464f6d2d5c54ffcb19f45a3
kodapassey <kodapassey@charter.net>
2022-07-19 20:24:13 -0700

    added descriptive comments for easy readibility

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
+// require for express
 const express = require('express');
 const fs = require('fs');
+// declaring app variable as express
 const app = express();
 const PORT = process.env.PORT || 3001;
 const path = require('path');
 const allNotes = require('./db/db.json');
 
 app.use(express.json());
+// renders public folder with html, css, and js
 app.use(express.static('public'));
 
+// get request for /notes that renders notes.html page
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
@@ -16,22 +20,27 @@ app.get('/api/notes', (req, res) => {
     res.json(allNotes.slice(1));
 });
 
+// post route for notes that creates notes
 app.post('/api/notes', (req, res) => {
     const newNote = createNote(req.body, allNotes);
 
     res.json(newNote);
 });
 
+// delete route
 app.delete('/api/notes/:id', (req, res) => {
     deleteNote(req.params.id, allNotes);
     res.json(true);
 });
 
+// catch all route for any unknown route that renders home page
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'))
 });
 
+// function to create a new note
 function createNote(body, notesArray) {
+    // sets newNote variable to body of post request
     const newNote = body;
     if (!Array.isArray(notesArray))
         notesArray = [];
@@ -51,7 +60,9 @@ function createNote(body, notesArray) {
     return newNote;
 };
 
+// function to delete a note
 function deleteNote(id, notesArray) {
+    // for loop that loops through each item of notesArray
     for (let i = 0; i < notesArray.length; i++) {
         let note = notesArray[i];
 
@@ -65,7 +76,7 @@ function deleteNote(id, notesArray) {
     }
 };
 
-
+// app.listen with console.log of what port it is on
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
