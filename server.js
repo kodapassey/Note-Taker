c3b13c7f4b14389415184170a98caa03c5e3d509
kodapassey <kodapassey@charter.net>
2022-07-19 20:00:29 -0700

    new note function created

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const PORT = process.env.PORT || 3001;
+const path = require('path');
+const allNotes = require('./db/db.json');
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    res.json(allNotes.slice(1));
+});
+
+app.post('/api/notes', (req, res) => {
+    const newNote = createNote(req.body, allNotes);
+
+    res.json(newNote);
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+});
+
+function createNote(body, notesArray) {
+    const newNote = body;
+    if (!Array.isArray(notesArray))
+        notesArray = [];
+
+    if (notesArray.length === 0)
+        notesArray.push(0);
+
+    body.id = notesArray[0];
+    notesArray[0]++;
+    body.id++;
+
+    notesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+    return newNote;
+};
+
+function deleteNote() {
+    
+}
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
